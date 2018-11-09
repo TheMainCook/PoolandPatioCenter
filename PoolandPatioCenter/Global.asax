13c5e59a3ad6e2167311facd3dabd2b84d5d1921
michael cook <michaeldcookjr40@gmail.com>
2018-11-08 22:32:10 -0600

    Add project files.

---
+++ PoolandPatioCenter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PoolandPatioCenter.MvcApplication" Language="C#" %>
---
+++ PoolandPatioCenter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoolandPatioCenter.Startup))]
+namespace PoolandPatioCenter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
