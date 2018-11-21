6cf07249f4795de13425df8d0383ef672b6a7801
michael cook <michaeldcookjr40@gmail.com>
2018-11-20 21:22:58 -0600

    updated navbar and started to added oAuth

--- PoolandPatioCenter/Controllers/ProductsController.cs
+++ PoolandPatioCenter/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using PoolandPatioCenter.Models;
 namespace PoolandPatioCenter.Controllers
 {
+    [AllowAnonymous]
     public class ProductsController : Controller
     {
         // GET: Products
