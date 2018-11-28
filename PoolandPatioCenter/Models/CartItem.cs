289273713d681bb48f9c3f7ee4d7e4a35525d300
michael cook <michaeldcookjr40@gmail.com>
2018-11-27 23:14:08 -0600

    updated Database

---
+++ PoolandPatioCenter/Models/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PoolandPatioCenter.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int CartItemQuantity { get; set; }
+        [ForeignKey("Cart")]
+        public int CartId { get; set; }
+        public Cart Cart { get; set; }
+        [ForeignKey("Products")]
+        public int ProductId { get; set; }
+        public Products Products { get; set; }
+    }
+}
--- PoolandPatioCenter/Models/LastBought.cs
+++ PoolandPatioCenter/Models/LastBought.cs
@@ -6,12 +6,16 @@ using System.Web;
 
 namespace PoolandPatioCenter.Models
 {
-    public class Cart
+    public class LastBought
     {
         public int Id { get; set; }
-        
+        public DateTime LastBoughtDate { get; set; }
+        [ForeignKey("Products")]
+        public int ProductsId { get; set; }
+        public Products Products { get; set; }
         public string UserId { get; set; }
         [ForeignKey("UserId")]
         public virtual ApplicationUser ApplicationUser { get; set; }
+
     }
 }
