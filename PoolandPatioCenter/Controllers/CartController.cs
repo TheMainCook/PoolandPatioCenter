93c55fa75f74ae47588a55da515879fa71a78177
michael cook <michaeldcookjr40@gmail.com>
2018-11-29 21:57:06 -0600

    updated cart view with cartitem api

--- PoolandPatioCenter/Controllers/CartController.cs
+++ PoolandPatioCenter/Controllers/CartController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PoolandPatioCenter.Models;
+using System;
 using System.Collections.Generic;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +11,23 @@ namespace PoolandPatioCenter.Controllers
 {
     public class CartController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CartController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Cart
         public ActionResult Index()
         {
-            return View();
+            var id = User.Identity.GetUserId();
+            var cartitem = _context.CartItem.Include(c => c.Products).ToList().Where(c => c.UserId == id);
+            return View(cartitem);
         }
     }
 }
